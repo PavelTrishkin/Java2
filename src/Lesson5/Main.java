8407e7f95925c264ff3df6daf5b9165aff713005
Павел <p-trishkin1@mail.ru>
2020-03-20 19:56:43 +0300

    HomeWork5

--- src/Lesson5/Main.java
+++ src/Lesson5/Main.java
@@ -19,6 +19,7 @@ public class Main {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
 
+        System.out.println(arr[15000]);
         System.out.println("Один поток справился за " + (System.currentTimeMillis() - start) + " миллисекунд");
     }
 
@@ -31,21 +32,22 @@ public class Main {
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
 
-        new Thread(() -> {
-            for (int i = 0; i < a1.length; i++) {
-                a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-        }).start();
+        MyThread m1 = new MyThread(a1);
+        MyThread m2 = new MyThread(a2);
 
-        new Thread(() -> {
-            for (int i = 0; i < a2.length; i++) {
-                a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            }
-        }).start();
+
+        try {
+            m1.join();
+            m2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
 
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h, h);
 
+        System.out.println(arr[15000]);
         System.out.println("Два потока справились за " + (System.currentTimeMillis() - start) + " миллисекунд");
     }
 
